1cc036c6e16e8193b6aabdc2489ce3436092d17c
Thomas Haschka <haschka@gmail.com>
2023-02-22 23:30:34 +0100

    Add files via upload

--- fasta-mode.el
+++ fasta-mode.el
@@ -16,34 +16,41 @@
 	  (seq_name nil)
 	  (line_start_pos nil)
 	  (line_end_pos nil))
-      (save-excursion
-	(while (eq nil (char-equal begin_char (string-to-char ">")))
-	  (setq count (+ count (- orig (point))))
-	  (setq orig (point))
-	  (move-beginning-of-line 1)
-	  (if (eq nil (char-after))
-	      (setq begin_char (string-to-char "0"))
-	    (setq begin_char (char-after)))
-	  (if (eq nil (eq 1 (point)))
-	      (progn 
-		(setq orig (- orig 1))
-		(backward-char 1))))
+      (if (< 1 (point))
+	  (progn
+	    (save-excursion
+	      (while (and (eq nil (char-equal begin_char (string-to-char ">")))
+			  (< 1 (point)))
+		(setq count (+ count (- orig (point))))
+		(setq orig (point))
+		(move-beginning-of-line 1)
+		(if (eq nil (char-after))
+		    (setq begin_char (string-to-char "0"))
+		  (setq begin_char (char-after)))
+		(if (eq nil (eq 1 (point)))
+		    (progn 
+		      (setq orig (- orig 1))
+		      (backward-char 1))))
 					; fix last line
-	(setq count (+ count 1))
-	(if (eq nil (eq 1 (point)))
-	    (forward-char 1))
-	(setq line_start_pos (point))
-	(setq line_end_pos (line-end-position))
-	(if (< 12 (- line_end_pos line_start_pos))
-	    (setq seq_name
-		  (concat (buffer-substring-no-properties (+ (point) 1)
-							  (+ (point) 11))
-			  "..."))
-	  (setq seq_name (buffer-substring-no-properties (+ line_start_pos 1)
-							 line_end_pos))))
+	    (setq count (+ count 1))
+	    (if (eq nil (eq 1 (point)))
+		(forward-char 1))
+	    (setq line_start_pos (point))
+	    (setq line_end_pos (line-end-position))
+	    (if (< 12 (- line_end_pos line_start_pos))
+		(setq seq_name
+		      (concat (buffer-substring-no-properties (+ (point) 1)
+							      (+ (point) 11))
+			      "..."))
+	      (setq seq_name (buffer-substring-no-properties
+			      (+ line_start_pos 1)
+			      line_end_pos))))
+	    (setq local-position
+		  (list "SeqPos: " (number-to-string count) " " seq_name)))
 	
-      (setq local-position
-	    (list "SeqPos: " (number-to-string count) " " seq_name))
+	(setq local-position
+	      (list "SeqPos: unk ")))
+      
       
       (setq mode-line-format
 	    '("%e" mode-line-front-space
